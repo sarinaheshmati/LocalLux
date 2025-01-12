ace306e2a5feacf6a28e3e484f1ee689be0e67bc
Parsaaw <101434381+Parsaaw@users.noreply.github.com>
2025-01-12 17:51:45 +0330

    add userSchema and setup

--- models/user.js
+++ models/user.js
@@ -1,3 +1,13 @@
 const mongoose	= require("mongoose"),
 	  passportLocalMongoose = require("passport-local-mongoose");
 
+// SCHEMA SETUP
+var userSchema = mongoose.Schema({
+	username: String,
+	password: String,
+	firstname: String,
+	lastname: String,
+	email: String,
+	phone_number: String,
+	app_role: [ String ],			// anonymous guest, tenant, host, admin
+	
