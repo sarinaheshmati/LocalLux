49dc865d6b468873d3d28103d7c3497667f2f277
Parsa Ali Khani <parsaalikhani@Parsas-MacBook-Pro.local>
2025-01-13 00:46:22 +0330

    Check if the renting dates are valid

--- routes/apartments.js
+++ routes/apartments.js
@@ -58,3 +58,11 @@ router.post("/", middleware.isLoggedIn, upload.array("images", 30)), async(req,r
 	
 		i += 1;
 	}}
+
+	
+		// Check if the renting dates are valid
+		var today = new Date();
+		var dd = String(today.getDate()).padStart(2, '0');
+		var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+		var yyyy = today.getFullYear();
+		today = yyyy + '-' + mm + '-' + dd;
