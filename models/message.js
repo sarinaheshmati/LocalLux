2591c1bd748d658133bb8de4027ee9074d5b93d1
Parsaaw <101434381+Parsaaw@users.noreply.github.com>
2025-01-12 17:20:48 +0330

    adding messageSchema

--- models/message.js
+++ models/message.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
+
+var messageSchema = mongoose.Schema({
+	subject: String,
+	content: String,
+	date: String,
+	sender: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User"
+	},
+	recipient: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User"
+	}
+});
+
 
