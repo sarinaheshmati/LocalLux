84e3c55c649323df0b818495502a8ae2ea6fdff8
Parsaaw <101434381+Parsaaw@users.noreply.github.com>
2025-01-12 17:31:16 +0330

    adding reviewSchema

--- models/review.js
+++ models/review.js
@@ -1,3 +1,13 @@
 const mongoose = require("mongoose");
 
 
+var reviewSchema = mongoose.Schema({
+	about: String,
+	rating: Number,
+	text: String,
+	date: String,
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User"
+	}
+});
