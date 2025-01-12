db2ad1545477508b5fccc7e21d318cb9a30dc73e
Parsaaw <101434381+Parsaaw@users.noreply.github.com>
2025-01-12 15:45:12 +0330

    wiriting apartmentSchema

--- models/apartment.js
+++ models/apartment.js
@@ -1,2 +1,13 @@
 const mongoose = require("mongoose");
 
+var apartmentSchema = mongoose.Schema({
+	name: String,
+	place: {
+		bedrooms: Number,
+		beds: Number,
+		bathrooms: Number,
+		room_type: String,	// private room, shared room, apartment
+		living_room: { type: String, default: 'False'},
+		floor: String,
+		area: Number
+	},
