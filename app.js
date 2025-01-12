ba5cdf904eff820b6c72deb28c6079c0f52f79d5
Sarina Heshmati <heshmatiisarina@gmail.com>
2025-01-12 14:14:52 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
