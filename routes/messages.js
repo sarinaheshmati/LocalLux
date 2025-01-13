c3cf21e19370cc366f066db421115f991fbb500c
Parsa Ali Khani <parsaalikhani@Parsas-MacBook-Pro.local>
2025-01-13 13:49:43 +0330

    show route for tenant

--- routes/messages.js
+++ routes/messages.js
@@ -23,3 +23,46 @@ router.get("/tenant/page/:pageNum", function(req,res){
 										 num_days: req.query.num_days, check_in: req.query.check_in,
 										 guests: req.query.guests, check_out: req.query.check_out });
 });
+
+
+// Show Route for tenant
+router.get("/tenant/:tenant_id/:apartment", middleware.isTenant, function(req,res){
+	User.findById(req.params.tenant_id).populate("messages.apartment")
+	.populate({ path:"messages.conversation", populate: { path: "sender recipient" }})
+	.exec(function(err, tenant){
+		if(err){
+			req.flash("error", err.message);
+			return res.redirect("back");
+		}
+
+		Apartment.findById(req.params.apartment).populate("host").exec(function(err, apartment){
+			if(err){
+				req.flash("error", err.message);
+				return res.redirect("back");
+			}
+
+			var conversation = [];
+			for(var mail of tenant.messages){
+				if(mail.apartment._id.equals(apartment._id)){
+					conversation = mail.conversation.concat([]);
+					break;
+				}
+			}
+
+			var str_apartment = JSON.stringify(apartment);
+			var str_conversation = JSON.stringify(conversation);
+
+			res.redirect(url.format({
+				pathname: "/messages/tenant/page/1",
+				query: {
+					"str_apartment": str_apartment,
+					"str_conversation": str_conversation,
+					"num_days": req.query.num_days,
+					"check_in": req.query.check_in,
+					"guests": req.query.guests,
+					"check_out": req.query.check_out
+				}
+			}));
+		});
+	});
+});
