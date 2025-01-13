fc46cee46325e5fd8260d4208841b89c0d5e67a8
Parsa Ali Khani <parsaalikhani@Parsas-MacBook-Pro.local>
2025-01-13 13:00:55 +0330

    Update Route

--- routes/apartments.js
+++ routes/apartments.js
@@ -215,3 +215,228 @@ router.get("/:id/edit", middleware.checkApartmentOwnership, function(req,res){
 		}
 	});
 });
+
+
+// Update Route
+router.put("/:id",  middleware.checkApartmentOwnership, upload.array("images", 10), function(req,res){
+	apartment.findById(req.params.id).populate("host").populate("reservations").populate("reviews")
+	.exec(async function(err, foundApartment){
+
+		req.body.apartment["images"] = foundApartment.images.concat([]);
+
+		// Remove selected images, if there is any
+		if(req.body.deleteImages && req.body.deleteImages.length){
+			// For each image selected to be removed
+			for(const public_id of req.body.deleteImages){
+				// Destroy access to the image on cloudinary
+				await cloudinary.v2.uploader.destroy(public_id);
+				// Remove it from apartment's images
+				for(const image of req.body.apartment.images){
+					if(image.public_id == public_id){
+						var index = req.body.apartment.images.indexOf(image);
+						var part1 = req.body.apartment.images.slice(0, index);
+						var part2 = req.body.apartment.images.slice(index+1,
+																	req.body.apartment.images.length);
+						req.body.apartment.images = part1.concat(part2);
+					}
+				}
+			}
+		}
+
+		// If one or more images were uploaded
+		if(req.files){
+			var	i=0;
+			// For each uploaded image
+			for(const file of req.files){
+				var image = await cloudinary.v2.uploader.upload(file.path);
+				// The firstly uploaded image should be the apartment's main image
+				if(i == 0 && req.body.new_main_image){
+					req.body.apartment["main_image"] = {
+						url: image.secure_url,
+						public_id: image.public_id
+					};
+				}else{
+					req.body.apartment.images.push({
+						url: image.secure_url,
+						public_id: image.public_id
+					});
+				}
+
+				i += 1;
+			}
+		}
+
+		// If the room type is still the old one
+		if(typeof req.body.room_type == 'undefined'){
+			req.body.place.room_type = foundApartment.place.room_type;
+		}
+
+		var today = new Date();
+		var dd = String(today.getDate()).padStart(2, '0');
+		var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+		var yyyy = today.getFullYear();
+		today = yyyy + '-' + mm + '-' + dd;
+
+		req.body.apartment["availability"] = [];
+		var entries = Object.entries(req.body.availability);
+		var	d1, d2, from, to;
+
+		// For each from-to date pair
+		for(var i=0; i<foundApartment.availability.length; i++){
+			var dates = {
+				from: foundApartment.availability[i].from,
+				to: foundApartment.availability[i].to
+			};
+
+			[from,d1] = entries.find(([from,d1]) => from == "from_"+i);
+			[to,d2] = entries.find(([to,d2]) => to == "to_"+i);
+
+			// If any of the availability dates has changed
+			if(dates.from.valueOf() != d1.valueOf() ||
+			   dates.to.valueOf() != d2.valueOf()){
+				// Check if the renting dates are valid
+				if(d1.valueOf() < today.valueOf() || d2.valueOf() < today.valueOf() ||
+				   d1.valueOf() > d2.valueOf()){
+					req.flash("error", "Availability dates should be valid. Please try again.");
+					return res.redirect("/apartments/" + foundApartment._id + "/edit");
+				};
+
+				if(dates.from.valueOf() != d1.valueOf()){
+					dates.from = d1;
+				};
+
+				if(dates.to.valueOf() != d2.valueOf()){
+					dates.to = d2;
+				};
+			};
+
+			req.body.apartment.availability.push(dates);
+		};
+
+		// If new dates have been added
+		if(req.body.more_dates_from && req.body.more_dates_to){
+			d1 = req.body.more_dates_from;
+			d2 = req.body.more_dates_to;
+
+			// Check if the new dates are valid
+			if(d1.valueOf() < today.valueOf() || d2.valueOf() < today.valueOf() ||
+			   d1.valueOf() > d2.valueOf()){
+				req.flash("error", "Availability dates should be valid. Please try again.");
+				return res.redirect("/apartments/" + foundApartment._id + "/edit");
+			};
+
+			dates = {
+				from: d1,
+				to:   d2
+			};
+
+			req.body.apartment.availability.push(dates);
+		};
+
+		if(req.body.apartment.availability.length > 0){
+			req.body.apartment.availability.sort(function({ from: f1, to: t1 },{ from: f2, to: t2 }){
+			var d1 = Date.parse(f1),
+				d2 = Date.parse(f2),
+				one_day = 1000*60*60*24,
+				diff = Math.round((d2-d1)/one_day);
+
+				return diff < 0;
+			});
+		};
+
+		// Get apartment object from new.ejs
+		req.body.apartment["place"] = Object.assign({}, req.body.place);
+		req.body.apartment["renting_rules"] = Object.assign({}, req.body.renting_rules);
+		req.body.apartment["facilities"] = Object.assign({}, req.body.facilities);
+		req.body.apartment["location"] = Object.assign({}, req.body.location);
+		req.body.apartment["host"] = Object.assign({}, req.user._doc);
+		req.body.apartment["reservations"] = foundApartment.reservations.concat([]);
+		req.body.apartment["reviews"] = foundApartment.reviews.concat([]);
+
+		// Use default values if needed
+		var	tempApartment = new apartment({});
+		if(!req.body.apartment.place.living_room){
+			req.body.apartment.place.living_room = tempApartment.place.living_room;
+		}
+		if(!req.body.apartment.renting_rules.smoking){
+			req.body.apartment.renting_rules.smoking = tempApartment.renting_rules.smoking;
+		}
+		if(!req.body.apartment.renting_rules.pets){
+			req.body.apartment.renting_rules.pets = tempApartment.renting_rules.pets;
+		}
+		if(!req.body.apartment.renting_rules.events){
+			req.body.apartment.renting_rules.eventse = tempApartment.renting_rules.events;
+		}
+		if(!req.body.apartment.facilities.wifi){
+			req.body.apartment.facilities.wifi = tempApartment.facilities.wifi;
+		}
+		if(!req.body.apartment.facilities.air_conditioning){
+			req.body.apartment.facilities.air_conditioning = tempApartment.facilities.air_conditioning;
+		}
+		if(!req.body.apartment.facilities.heating){
+			req.body.apartment.facilities.heating = tempApartment.facilities.heating;
+		}
+		if(!req.body.apartment.facilities.kitchen){
+			req.body.apartment.facilities.kitchen = tempApartment.facilities.kitchen;
+		}
+		if(!req.body.apartment.facilities.tv){
+			req.body.apartment.facilities.tv = tempApartment.facilities.tv;
+		}
+		if(!req.body.apartment.facilities.parking){
+			req.body.apartment.facilities.parking = tempApartment.facilities.parking;
+		}
+		if(!req.body.apartment.facilities.elevator){
+			req.body.apartment.facilities.elevator = tempApartment.facilities.elevator;
+		}
+
+		for([key, value] of Object.entries(req.body)){
+			if(typeof value == 'string' && value == 'reverse_geocoding'){
+				req.body.apartment.location.address = key;
+				break;
+			}
+		}
+		
+		geocoder.geocode(req.body.apartment.location.address, function(err, data){
+    		if(err || !data.length){
+				req.flash('error', 'Invalid address');
+    			return res.redirect('back');
+			} 
+
+			req.body.apartment.location.lat 		=    	data[0].latitude;
+			req.body.apartment.location.lng  		=     	data[0].longitude;
+
+			req.body.apartment.location.address = tr(req.body.apartment.location.address);	//Making Normal Geocoding and Reverse in english format
+
+			// Find current user in db
+			User.findById(req.user._id, function(err, user){
+				if(err){
+					req.flash("error", err.message);
+					res.redirect("/users/" + user._id + "/host");
+				}else if(!user){
+					req.flash("error", "User not found");
+					res.redirect("back");
+				}else{
+					// Update apartment in db
+					apartment.findByIdAndUpdate(foundApartment._id, req.body.apartment, function(err, apartment){
+						if(err){
+							req.flash("error", err.message);
+							res.redirect("/users/" + user._id + "/host");
+						}else{
+							apartment.save();
+							var i=0;
+							for(var place in user.apartments){
+								if(place._id == apartment._id){
+									user.apartments[i] = Object.assign({}, apartment);
+								}
+								i += 1;
+							}
+							user.save();
+							req.flash("success", "Updated " + apartment.name + " successfully!");
+							res.redirect("/apartments/" + apartment._id);
+							}
+					});
+				}
+			});
+		});
+	});
+});
