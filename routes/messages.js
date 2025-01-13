f0c0dfb095c728cd09a43c72e4d989c7be4f107d
Parsa Ali Khani <parsaalikhani@Parsas-MacBook-Pro.local>
2025-01-13 13:48:55 +0330

    pagination

--- routes/messages.js
+++ routes/messages.js
@@ -5,3 +5,21 @@ const express	 = require("express"),
 	  Message 	 = require("../models/message"),
 	  middleware = require("../middleware"),
 	  url		 = require("url");
+
+// Pagination for tenant
+router.get("/tenant/page/:pageNum", function(req,res){
+
+	var apartment = JSON.parse(req.query.str_apartment);
+	var conversation = JSON.parse(req.query.str_conversation);
+	
+	var results_per_page = 10,
+		start			 = (req.params.pageNum - 1) * results_per_page;
+	
+	var paginated = conversation.slice(start,start + results_per_page);
+
+	res.render("messages/tenant/show", { conversation: paginated, all_messages: conversation,
+										 apartment: apartment, host: apartment.host,
+										 results_per_page: results_per_page, pageNum: req.params.pageNum,
+										 num_days: req.query.num_days, check_in: req.query.check_in,
+										 guests: req.query.guests, check_out: req.query.check_out });
+});
